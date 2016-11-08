4b1a845d05ab13234b20faf2ea6414a440c23e71
Raikia <raikiasec@gmail.com>
2016-11-08 18:17:30 +0000

    Added TargetList and TargetUser

---
+++ database/migrations/2016_11_08_181307_create_target_lists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTargetListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('target_lists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('target_lists');
+    }
+}
