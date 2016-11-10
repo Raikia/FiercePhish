726a755cd30aa0387dbb70a94434d2bdc1b1047d
Raikia <raikiasec@gmail.com>
2016-11-10 03:31:39 +0000

    Added file upload import for TargetUsers

--- app/Http/Controllers/TargetsController.php
+++ app/Http/Controllers/TargetsController.php
@@ -8,6 +8,8 @@ use App\Http\Requests;
 use App\TargetUser;
 use App\TargetList;
 
+use File;
+
 class TargetsController extends Controller
 {
     public function __construct()
@@ -19,5 +21,80 @@ class TargetsController extends Controller
     {
         $targetUsers = TargetUser::with('lists')->orderBy('last_name')->get();
         return view('targets.index')->with('targetUsers', $targetUsers);
+    }
+    
+    public function addTarget(Request $request)
+    {
+        $this->validate($request, [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'email' => 'required|email'
+        ]);
+        $checkUser = TargetUser::where('first_name', $request->input('first_name'))->where('last_name', $request->input('last_name'))->where('email', $request->input('email'))->get();
+        if (count($checkUser) == 0)
+        {
+            TargetUser::create($request->all());
+            return back()->with('success', 'Target added successfully');
+        }
+        else
+        {
+            return back()->withErrors('Target already exists');
+        }
+    }
+    
+    public function importTargets(Request $request)
+    {
+        $this->validate($request, [
+            'import_file' => 'required|mimes:csv,txt|max:15000'
+        ]);
+        $content = File::get($request->file('import_file')->getRealPath());
+        $lines = explode("\n", $content);
+        $errors = [];
+        $success = 0;
+        $warn = 0;
+        $lineNum = 0;
+        foreach ($lines as $line)
+        {
+            ++$lineNum;
+            $line = trim($line);
+            if ($line == "")
+                continue;
+            $parts = explode(",", $line);
+            for ($x=0; $x<count($parts); ++$x)
+            {
+                $parts[$x] = trim(trim($parts[$x]),'"');
+            }
+            if (count($parts) < 3)
+                $errors[] = $lineNum;
+            else
+            {
+                if (strpos($parts[2],'@') === false)
+                    $errors[] = $lineNum;
+                else
+                {
+                    $t = new TargetUser();
+                    $t->first_name = $parts[0];
+                    $t->last_name = $parts[1];
+                    $t->email = $parts[2];
+                    try
+                    {
+                        $t->save();
+                        ++$success;
+                    }
+                    catch (\Illuminate\Database\QueryException $e)
+                    {
+                        ++$warn;
+                    }
+                }
+            }
+        }
+        $ret_obj = back();
+        if ($success != 0)
+            $ret_obj = $ret_obj->with('success', 'Successfully added ' . $success . ' target'.(($success==1)?'':'s'));
+        if ($warn != 0)
+            $ret_obj = $ret_obj->with('warn', 'Did not add '.$warn.' duplicate entr'.(($warn==1)?'y':'ies'));
+        if (count($errors) > 0)
+            $ret_obj = $ret_obj->withErrors('Unable to add targets from line'.((count($errors)==1)?'':'s').' ' . implode(', ', $errors));
+        return $ret_obj;
     }
 }
--- resources/views/targets/index.blade.php
+++ resources/views/targets/index.blade.php
@@ -6,43 +6,13 @@
   <div class="title_left">
     <h3>List of Targets</h3>
   </div>
-<!--
-  <div class="title_right">
-    <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search for...">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button">Go!</button>
-        </span>
-      </div>
-    </div>
-  </div>-->
 </div>
 
 <div class="clearfix"></div>
 
 <div class="row">
   <div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel"><!--
-      <div class="x_title">
-        <h2>Plain Page</h2>
-        <ul class="nav navbar-right panel_toolbox">
-          <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Settings 1</a>
-              </li>
-              <li><a href="#">Settings 2</a>
-              </li>
-            </ul>
-          </li>
-          <li><a class="close-link"><i class="fa fa-close"></i></a>
-          </li>
-        </ul>
-        <div class="clearfix"></div>
-      </div>-->
+    <div class="x_panel">
       <div class="x_content">
           <table id="datatable" class="table table-striped table-bordered datatable">
               <thead>
@@ -86,4 +56,128 @@
 </div>
 
 
+<!----------------- -->
+
+<div class="row">
+  <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="x_panel">
+      <div class="x_title">
+        <h2><i class="fa fa-plus"></i> Add a Target</h2>
+        <div class="clearfix"></div>
+      </div>
+      <div class="x_content">
+
+        <form class="form-horizontal form-label-left" method="post" action="{{ action('TargetsController@addTarget') }}">
+          {{ csrf_field() }}
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first_name">First Name <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="first_name" name="first_name" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last_name">Last Name <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="last_name" name="last_name" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Email <span class="required">*</span></label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input id="email" class="form-control col-md-7 col-xs-12" required="required" type="text" name="email">
+            </div>
+          </div>
+
+          <div class="ln_solid"></div>
+          <div class="form-group">
+            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+              <button type="button" id="add_target_clear_btn" class="btn btn-primary">Clear</button>
+              <button type="submit" class="btn btn-success">Create Target</button>
+            </div>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="x_panel">
+      <div class="x_title">
+        <h2><i class="fa fa-download"></i> Import Targets</h2>
+        <div class="clearfix"></div>
+      </div>
+      <div class="x_content">
+
+        <form class="form-horizontal form-label-left" enctype="multipart/form-data" method="post" action="{{ action('TargetsController@importTargets') }}">
+          {{ csrf_field() }}
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="import_file">Import File  <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="input-group">
+                    <span class="input-group-btn">
+                        <label class="btn btn-primary"><i class="fa fa-file-o"></i><input type="file" name="import_file" id="import_file" style="visibility: hidden; position:absolute;" /></label>
+                    </span>
+                    <input type="text" id="selectedFile" class="form-control" readonly />
+                </div>
+                
+              <!--<label class="btn btn-default btn-file">
+                  Browse <input type="file" style="display: none;" />
+              </label>-->
+              <!--<input type="file" id="first_name" name="first_name" required="required" class="form-control col-md-7 col-xs-12">-->
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first_name">CSV Format 
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="input-group">
+                    <p style="margin-top: 8px;">First Name, Last Name, Email</p>
+                </div>
+              <!--<label class="btn btn-default btn-file">
+                  Browse <input type="file" style="display: none;" />
+              </label>-->
+              <!--<input type="file" id="first_name" name="first_name" required="required" class="form-control col-md-7 col-xs-12">-->
+            </div>
+          </div>
+          
+          <div class="ln_solid"></div>
+          <div class="form-group">
+            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+              <button type="button" id="import_file_clear_btn" class="btn btn-primary">Clear</button>
+              <button type="submit" class="btn btn-success">Create Target</button>
+            </div>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
+
+@section('footer')
+<script type="text/javascript">
+    $("#add_target_clear_btn").click(function() {
+        $("#first_name").val('');
+        $("#last_name").val('');
+        $("#email").val('');
+    });
+    
+    $("#import_file").change(function() {
+        $("#selectedFile").val($(this).val());
+    });
+    
+    $("#import_file_clear_btn").click(function() {
+        $("#import_file").val('');
+        $("#selectedFile").val('');
+    });
+</script>
 @endsection
