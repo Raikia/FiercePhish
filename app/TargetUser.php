43bab4f4aa42ac4a88d7701538469d952b94a210
Raikia <raikiasec@gmail.com>
2016-11-12 00:52:57 +0000

    Fixed TargetUser so uuid is saved when it is first read

--- app/TargetUser.php
+++ app/TargetUser.php
@@ -16,6 +16,9 @@ class TargetUser extends Model
     
     public function uuid()
     {
-        return sha1('xG!1jBdn?/y]n=~07DRp'.$this->first_name.'.M{5>gDe'.$this->last_name.'`lWcv'.$this->email.'q=N8{?iW1V[,15^B*IRC');
+        if ($this->uuid == '' || $this->uuid == null)
+            $this->uuid = sha1('xG!1jBdn?/y]n=~07DRp'.$this->first_name.'.M{5>gDe'.$this->last_name.'`lWcv'.$this->email.'q=N8{?iW1V[,15^B*IRC');
+            $this->save();
+        return $this->uuid;
     }
 }
--- database/migrations/2016_11_08_181254_create_target_users_table.php
+++ database/migrations/2016_11_08_181254_create_target_users_table.php
@@ -18,6 +18,7 @@ class CreateTargetUsersTable extends Migration
             $table->string('last_name');
             $table->string('email');
             $table->text('notes');
+            $table->string('uuid');
             $table->unique(['first_name','last_name','email']);
             $table->timestamps();
         });
