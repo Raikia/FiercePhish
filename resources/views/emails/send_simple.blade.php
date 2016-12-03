848333af5acbc1df1ca455634d40280385744066
Chris King <raikiasec@gmail.com>
2016-12-03 11:51:18 -0600

    Added simple send and Email class

--- resources/views/emails/send_simple.blade.php
+++ resources/views/emails/send_simple.blade.php
@@ -18,26 +18,26 @@
               <div class="form-group">
                 <label class="control-label col-md-3 col-sm-3 col-xs-12">Sender Name <span class="required">*</span></label>
                 <div class="col-md-9 col-sm-9 col-xs-12">
-                  <input type="text" id="sender_name" name="sender_name" class="form-control" placeholder="Bill Smith">
+                  <input type="text" id="sender_name" name="sender_name" class="form-control" placeholder="Bill Smith" value="{{ old('sbt_sender_name') }}">
                 </div>
               </div>
               <div class="form-group">
                 <label class="control-label col-md-3 col-sm-3 col-xs-12">Sender Email <span class="required">*</span></label>
                 <div class="col-md-9 col-sm-9 col-xs-12">
-                  <input type="email" id="sender_email" name="sender_email" class="form-control" placeholder="bsmith@malicious.com">
+                  <input type="email" id="sender_email" name="sender_email" class="form-control" placeholder="bsmith@malicious.com" value="{{ old('sbt_sender_email') }}">
                 </div>
               </div>
               <div class="ln_solid"></div>
               <div class="form-group">
                 <label class="control-label col-md-3 col-sm-3 col-xs-12">Receiver Name <span class="required">*</span></label>
                 <div class="col-md-9 col-sm-9 col-xs-12">
-                  <input type="text" id="receiver_name" name="receiver_name" class="form-control" placeholder="John Doe">
+                  <input type="text" id="receiver_name" name="receiver_name" class="form-control" placeholder="John Doe" value="{{ old('sbt_receiver_name') }}">
                 </div>
               </div>
               <div class="form-group">
                 <label class="control-label col-md-3 col-sm-3 col-xs-12">Receiver Email <span class="required">*</span></label>
                 <div class="col-md-9 col-sm-9 col-xs-12">
-                  <input type="email" id="receiver_email" name="receiver_email" class="form-control" placeholder="john.doe@domain.com">
+                  <input type="email" id="receiver_email" name="receiver_email" class="form-control" placeholder="john.doe@domain.com" value="{{ old('sbt_receiver_email') }}">
                 </div>
               </div>
               <!--<div class="ln_solid"></div>
@@ -95,11 +95,11 @@
       <div class="form-group">
         <label class="control-label col-md-1 col-sm-1 col-xs-1">Subject <span class="required">*</span></label>
         <div class="col-md-9 col-sm-9 col-xs-12">
-          <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject...">
+          <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject..." value="{{ old('sbt_subject') }}">
         </div>
       </div>
 
-      <textarea rows="10" id="bodyMsg" name="bodyMsg" class="bodyMsg"></textarea>
+      <textarea rows="10" id="bodyMsg" name="bodyMsg" class="bodyMsg">{{ old('sbt_message') }}</textarea>
     </form>
     </div>
   </div>
@@ -108,7 +108,7 @@
 <div class="row">
   <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="x_panel">
-      <form id="send_email_form" class="form-horizontal form-label-left input_mask" action="{{ action('EmailController@send_simple_post') }}" method="post">
+      <form id="send_email_form" class="form-horizontal form-label-left input_mask" enctype="multipart/form-data" action="{{ action('EmailController@send_simple_post') }}" method="post">
         {{ csrf_field() }}
         <div style="text-align: center;">
           <button class="btn btn-success" style="margin-right: 30px;">Send email</button>
