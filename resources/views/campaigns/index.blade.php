41a85b77ce703f7e2e95e600d985b8d9b848fbc4
Raikia <raikiasec@gmail.com>
2016-12-11 21:30:34 +0000

    Added overview of all campaigns

--- resources/views/campaigns/index.blade.php
+++ resources/views/campaigns/index.blade.php
@@ -12,8 +12,35 @@
 <div class="row">
   <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="x_panel">
+      <div class="x_title">
+        <h2>List of Campaigns</h2>
+        <div class="clearfix"></div>
+      </div>
       <div class="x_content">
-          This is a thing
+          <table class="table table-striped table-bordered datatable" id="mainCampaignTable">
+            <thead>
+              <tr>
+                <th>Campaign Name</th>
+                <th>Campaign Description</th>
+                <th>State</th>
+                <th>Email Status</th>
+                <th>Created</th>
+                <th>Last Update</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($all_campaigns as $camp)
+                <tr>
+                  <td>{{ $camp->name }}</td>
+                  <td>{{ $camp->description }}</td>
+                  <td>{{ $camp->getStatus() }}</td>
+                  <td>{{ $camp->emails()->where('status', \App\Email::SENT)->count() }} / {{ $camp->emails()->count() }} sent</td>
+                  <td>{{ $camp->created_at->format('M j, Y @ g:i:s a') }}</td>
+                  <td>{{ $camp->updated_at->format('M j, Y @ g:i:s a') }}</td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
       </div>
     </div>
   </div>
@@ -24,6 +51,7 @@
 
 @section('footer')
 <script type="text/javascript">
-  
+/* global $ */
+  $("#mainCampaignTable").dataTable();
 </script>
 @endsection
