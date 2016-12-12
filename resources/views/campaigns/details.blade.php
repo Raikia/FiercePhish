44fc87a9e2698808eb085d448f0621c423126553
Raikia <raikiasec@gmail.com>
2016-12-12 01:32:51 +0000

    Added email log for campaign

--- resources/views/campaigns/details.blade.php
+++ resources/views/campaigns/details.blade.php
@@ -10,17 +10,17 @@
 <div class="clearfix"></div>
 
 <div class="row">
-    <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="col-md-5 col-sm-5 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Campaign Overview</h2>
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
-                <table class="table table-striped table-bordered datatable" style="width: 100%; margin-left: auto; margin-right: auto;">
+                <table class="table table-striped table-bordered" style="width: 100%; margin-left: auto; margin-right: auto;">
                     <tbody>
                         <tr>
-                            <td style="width: 25%;"><b>Campaign Name</b></td>
+                            <td style="width: 27%;"><b>Campaign Name</b></td>
                             <td>{{ $campaign->name }}</td>
                         </tr>
                         <tr>
@@ -61,9 +61,38 @@
             </div>
         </div>
     </div>
+    <div class="col-md-7 col-sm-7 col-xs-12">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2>Email Log</h2>
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+                <table id="emailLogTable" class="table table-striped table-bordered datatable">
+                    <thead>
+                        <tr>
+                            <th>Receiver Name</th>
+                            <th>Receiver Email</th>
+                            <th>UUID</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($campaign->emails as $email)
+                            <tr>
+                                <td>{{ $email->receiver_name }}</td>
+                                <td>{{ $email->receiver_email }}</td>
+                                <td>{{ \App\TargetUser::where('first_name', explode(' ',$email->receiver_name)[0])->where('email', $email->receiver_email)->first()->uuid() }}</td>
+                                <td>{{ $email->getStatus() }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
-          
-          
+
           
           
           
@@ -78,5 +107,7 @@
             $("#cancelForm").submit();
         });
     });
+    
+    $("#emailLogTable").dataTable();
 </script>
 @endsection
