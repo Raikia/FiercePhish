7f7d4755321a05cc45320398d7dd3205be1b72fa
Raikia <raikiasec@gmail.com>
2016-12-12 00:04:31 +0000

    Changed logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="logo">
-    <img src="{{ asset('images/login_logo.png') }}" style="width: 100px;" />
+    <img src="{{ asset('images/login_logo.png') }}" style="width: 70px;" />
 </div>
 <div class="login-page">
   <div class="form">
