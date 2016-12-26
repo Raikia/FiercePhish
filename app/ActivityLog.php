f5f9b87e84096d2e6ef1be7eaaf075a07f15f33e
Raikia <raikiasec@gmail.com>
2016-12-26 21:04:16 +0000

    Added activitylog model and migration

---
+++ app/ActivityLog.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App;
+
+use Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityLog extends Model
+{
+    public static function log($msg, $type="General")
+    {
+    	if (empty($msg))
+    		return;
+    	$a = new ActivityLog();
+    	$a->log = $msg;
+    	$a->type = $type;
+    	$a->is_error = false;
+    	if (Auth::check())
+    		$a->user_id = auth()->id;
+    	else
+    		$a->user_id = null;
+    	$a->save();
+    	return $a;
+    }
+
+    public function set_ref_id($id)
+    {
+    	$this->ref_id = $id;
+    	$this->save();
+    	return $this;
+    }
+
+    public function set_ref_text($text)
+    {
+    	$this->ref_text = $text;
+    	$this->save();
+    	return $this;
+    }
+
+    public function set_error($bool)
+    {
+    	$this->is_error = $bool;
+    	$this->save();
+    	return $this;
+    }
+
+    public function read()
+    {
+    	$ret_text = '';
+    	if ($this->is_error)
+    		$ret_text .= '!!!! ERROR !!!! - ';
+    	$ret_text .= '['.$this->type.'] ';
+    	$ret_text .= $this->log;
+    	$username = '  (Unknown User)';
+    	if ($this->user_id == null)
+    		$username = '';
+    	else
+    	{
+    		$usr = User::find($this->user_id);
+    		if ($usr != null)
+    			$username = '  ('.$usr->name.')';
+    	}
+    	$ret_text .= $username;
+    	return $ret_text;
+    }
+}
---
+++ database/migrations/2016_12_26_203439_create_activity_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivityLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ref_id')->nullable();
+            $table->string('ref_text')->nullable();
+            $table->string('type');
+            $table->boolean('is_error');
+            $table->integer('user_id')->nullable();
+            $table->text('log');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('activity_logs');
+    }
+}
