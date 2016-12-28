c81a84fbd1432f5d5c7be4848b41d540b0e52f86
Chris King <raikiasec@gmail.com>
2016-12-28 13:44:07 -0600

    Finished dashboard graphs, will probably need to change colors sometime though

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\ActivityLog;
+use DB;
+use App\Email;
+use App\Campaign;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -16,6 +20,38 @@ class DashboardController extends Controller
     }
     public function index()
     {
-        return view('dashboard.index')->with('activitylog', ActivityLog::fetch());
+    	// Get sent email stats
+    	$rawSendEmailData = DB::table('emails')->select(DB::raw('DATE(updated_at) as date'), DB::raw('count(*) as numEmails'))->groupBy('date')->where('status', Email::SENT)->get();
+    	// Fill in the gaps of dates where no emails were sent
+    	$sendEmailData = [];
+    	if (count($rawSendEmailData) > 0)
+    	{
+	    	$sendEmailData = [$rawSendEmailData[0]];
+	    	for ($x=1; $x<count($rawSendEmailData); ++$x)
+	    	{
+	    		$curDate = Carbon::parse($rawSendEmailData[$x-1]->date)->addDay(1)->format('Y-m-d');
+	    		while ($curDate != $rawSendEmailData[$x]->date)
+	    		{
+	    			$obj = new \stdClass();
+	    			$obj->date = $curDate;
+	    			$obj->numEmails = "0";
+	    			$sendEmailData[] = $obj;
+	    			$curDate = Carbon::parse($curDate)->addDay(1)->format('Y-m-d');
+	    		}
+	    		$sendEmailData[] = $rawSendEmailData[$x];
+	    	}
+	    }
+
+    	// Get error emails
+    	$errorEmailData = $rawSendEmailData = DB::table('emails')->select(DB::raw('DATE(updated_at) as date'), DB::raw('count(*) as numEmails'))->groupBy('date')->where('status', Email::CANCELLED)->get();
+
+    	// Get Statistics
+    	$statistics = [
+    		'numSent' => Email::where('status', Email::SENT)->count(),
+    		'numCancelled' => Email::where('status', Email::CANCELLED)->count(),
+    		'numPending' => Email::where('status', Email::NOT_SENT)->orWhere('status', Email::SENDING)->orWhere('status', Email::PENDING_RESEND)->count(),
+    	];
+
+        return view('dashboard.index')->with('activitylog', ActivityLog::fetch())->with('sendEmailData', $sendEmailData)->with('errorEmailData', $errorEmailData)->with('emailStats', $statistics)->with('allActiveCampaigns', Campaign::where('status', Campaign::NOT_STARTED)->orWhere('status', Campaign::SENDING)->orWhere('status', Campaign::WAITING)->get());
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -63,18 +63,26 @@
     <div class="x_panel">
       <div class="x_title">
         <h2>Email Sending Summary <small>Daily progress</small></h2>
-        <div class="filter">
-          <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-            <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
-          </div>
-        </div>
         <div class="clearfix"></div>
       </div>
       <div class="x_content">
         <div class="col-md-9 col-sm-12 col-xs-12">
           <div class="demo-container" style="height:280px">
-            <div id="emailGraph" class="demo-placeholder"></div>
+            <div id="emailGraph" style="height: 280px;"></div>
+            <div class="tiles">
+                        <div class="col-md-4 tile">
+                          <span>Emails Sent</span>
+                          <h2>{{ $emailStats['numSent'] }}</h2>
+                        </div>
+                        <div class="col-md-4 tile">
+                          <span>Emails Cancelled</span>
+                          <h2>{{ $emailStats['numCancelled'] }}</h2>
+                        </div>
+                        <div class="col-md-4 tile">
+                          <span>Emails Awaiting Sending</span>
+                          <h2>{{ $emailStats['numPending'] }}</h2>
+                        </div>
+                      </div>
           </div>
           
 
@@ -83,7 +91,7 @@
         <div class="col-md-3 col-sm-12 col-xs-12">
           <div>
             <div class="x_title">
-              <h2>Recent Emails Sent</h2>
+              <h2>Recently Sent Emails</h2>
               <div class="clearfix"></div>
             </div>
             <ul class="list-unstyled top_profiles scroll-view">
@@ -117,18 +125,17 @@
         <div class="row" style="border-bottom: 1px solid #E0E0E0; padding-bottom: 5px; margin-bottom: 5px;">
           <div class="col-md-12">
             <div class="row" style="text-align: center;">
+            @foreach ($allActiveCampaigns as $campaign)
               <div class="col-md-4">
-                <canvas id="canvas1i" height="110" width="110" style="margin: 5px 10px 10px 0"></canvas>
-                <h4 style="margin:0">Campaign 1</h4>
+                <div id="campaign{{ $campaign->id}}pie" style="height: 110px; width: 110px; margin-left: auto; margin-right: auto; margin-bottom: 10px;"></div>
+                <h4 style="margin:0; margin-bottom: 20px;">{{ $campaign->name }}</h4>
               </div>
-              <div class="col-md-4">
-                <canvas id="canvas1i2" height="110" width="110" style="margin: 5px 10px 10px 0"></canvas>
-                <h4 style="margin:0">Campaign 2</h4>
+            @endforeach
+            @if (count($allActiveCampaigns) == 0)
+              <div class="col-md-12">
+                <h2>No Active Campaigns</h2>
               </div>
-              <div class="col-md-4">
-                <canvas id="canvas1i3" height="110" width="110" style="margin: 5px 10px 10px 0"></canvas>
-                <h4 style="margin:0">Campaign 3</h4>
-              </div>
+            @endif
             </div>
           </div>
         </div>
@@ -159,170 +166,131 @@
 
 @section('footer')
 <script type="text/javascript">
-/*
-// Flot
-//define chart clolors ( you maybe add more colors if you want or flot will add it automatic )
-        var chartColours = ['#96CA59', '#3F97EB', '#72c380', '#6f7a8a', '#f7cb38', '#5a8022', '#2c7282'];
-
-        //generate random number for charts
-        randNum = function() {
-          return (Math.floor(Math.random() * (1 + 40 - 20))) + 20;
-        };
-
-        var d1 = [];
-        //var d2 = [];
-
-        //here we generate data for chart
-        for (var i = 0; i < 30; i++) {
-          d1.push([new Date(Date.today().add(i).days()).getTime(), randNum() + i + i + 10]);
-          //    d2.push([new Date(Date.today().add(i).days()).getTime(), randNum()]);
-        }
-
-        var chartMinDate = d1[0][0]; //first day
-        var chartMaxDate = d1[20][0]; //last day
-
-        var tickSize = [1, "day"];
-        var tformat = "%d/%m/%y";
-
-        //graph options
-        var options = {
-          grid: {
-            show: true,
-            aboveData: true,
-            color: "#3f3f3f",
-            labelMargin: 10,
-            axisMargin: 0,
-            borderWidth: 0,
-            borderColor: null,
-            minBorderMargin: 5,
-            clickable: true,
-            hoverable: true,
-            autoHighlight: true,
-            mouseActiveRadius: 100
-          },
-          series: {
-            lines: {
-              show: true,
-              fill: true,
-              lineWidth: 2,
-              steps: false
-            },
-            points: {
-              show: true,
-              radius: 4.5,
-              symbol: "circle",
-              lineWidth: 3.0
-            }
-          },
-          legend: {
-            position: "ne",
-            margin: [0, -25],
-            noColumns: 0,
-            labelBoxBorderColor: null,
-            labelFormatter: function(label, series) {
-              // just add some space to labes
-              return label + '&nbsp;&nbsp;';
-            },
-            width: 40,
-            height: 1
-          },
-          colors: chartColours,
-          shadowSize: 0,
-          tooltip: true, //activate tooltip
-          tooltipOpts: {
-            content: "%s: %y.0",
-            xDateFormat: "%d/%m",
-            shifts: {
-              x: -30,
-              y: -50
-            },
-            defaultTheme: false
-          },
-          yaxis: {
-            min: 0
-          },
-          xaxis: {
-            mode: "time",
-            minTickSize: tickSize,
-            timeformat: tformat,
-            min: chartMinDate,
-            max: chartMaxDate
-          }
-        };
-  var plot = $.plot($("#emailGraph"), [{
-          label: "Emails Sent",
-          data: d1,
-          lines: {
-            fillColor: "rgba(150, 202, 89, 0.12)"
-          }, //#96CA59 rgba(150, 202, 89, 0.42)
-          points: {
-            fillColor: "#fff"
-          }
-        }], options);
 
 
+$(document).ready(function() {
 
+/* Plot the graph for sent emails */
+  var dataSent = [
+  @foreach ($sendEmailData as $email)
+    [new Date("{{ $email->date }}"), {{ $email->numEmails }}],
+  @endforeach
+  ];
 
+  var dataError = [
+  @foreach ($errorEmailData as $email)
+    [new Date("{{ $email->date }}"), {{ $email->numEmails }}],
+  @endforeach
+  ];
 
- // Doughnut Chart
-  var canvasDoughnut,
-            options = {
-              legend: false,
-              responsive: false
-            };
-
-        new Chart(document.getElementById("canvas1i"), {
-          type: 'doughnut',
-          tooltipFillColor: "rgba(51, 51, 51, 0.55)",
-          data: {
-            labels: [
-              "Symbian",
-              "Blackberry",
-              "Other",
-              "Android",
-              "IOS"
-            ],
-            datasets: [{
-              data: [15, 20, 30, 10, 30],
-              backgroundColor: [
-                "#BDC3C7",
-                "#9B59B6",
-                "#E74C3C",
-                "#26B99A",
-                "#3498DB"
-              ],
-              hoverBackgroundColor: [
-                "#CFD4D8",
-                "#B370CF",
-                "#E95E4F",
-                "#36CAAB",
-                "#49A9EA"
-              ]
-
-            }]
-          },
-          options: options
-        });
-
-*/
-
-
-  var latestActivityLog = {{ ($activitylog->count() > 0)?$activitylog[0]->id:-1 }};
-  $(document).ready(function() {
-    window.setInterval(grabActivityLog, 2000);
-  });
-
-
-
-  function grabActivityLog()
-  {
-    $.get("{{ action('AjaxController@get_activitylog') }}/"+latestActivityLog, function(results) {
-      for (var x = 0; x < results.data.length; ++x)
-      {
-        $("#activityLog").prepend(results.data[x] + "<br />");
+  var dataset = [
+    {
+      label: "Emails Cancelled",
+      data: dataError,
+      color: "#FF0000",
+      points: {show: true},
+      lines: {show: false, fill: false}
+    },
+    {
+      label: "Emails Sent",
+      data: dataSent,
+      color: "#00FF00",
+      points: {show: true},
+      lines: {show: true}
+    },
+  ];
+  var options = {
+    xaxis: {
+      mode: "time",
+      minTickSize: [1, "day"],
+      timeformat: "%m/%e/%Y"
+    },
+    yaxis: {
+      minTickSize: 1,
+      tickDecimals: 0,
+    },
+    grid: {
+      show: true,
+      aboveData: true,
+      color: "#3f3f3f",
+      labelMargin: 10,
+      axisMargin: 0,
+      borderWidth: 0,
+      borderColor: null,
+      minBorderMargin: 5,
+      clickable: true,
+      hoverable: true,
+      autoHighlight: true,
+      mouseActiveRadius: 100
+    },
+    series: {
+      lines: {
+        show: true,
+        fill: true,
+        lineWidth: 2,
+        steps: false
+      },
+      points: {
+        show: true,
+        radius: 4.5,
+        symbol: "circle",
+        lineWidth: 3.0
       }
-      latestActivityLog = results.latest_id;
-    });
-  }
+    },
+  };
+  var plot = $.plot($("#emailGraph"), dataset, options);
+
+/* Plot the campaign pie charts */
+@foreach ($allActiveCampaigns as $campaign)
+  var camp{{ $campaign->id}}dataset = [
+    {
+      label: "Emails Cancelled",
+      data: {{ $campaign->emails()->where('status', \App\Email::CANCELLED)->orWhere('status', \App\Email::FAILED)->count() }},
+      color: "#FF0000"
+    },
+    {
+      label: "Emails Pending",
+      data: {{ $campaign->emails()->where('status', \App\Email::NOT_SENT)->orWhere('status', \App\Email::SENDING)->orWhere('status', \App\Email::PENDING_RESEND)->count() }},
+      color: "#0000FF"
+    },
+    {
+      label: "Emails Sent",
+      data: {{ $campaign->emails()->where('status', \App\Email::SENT)->count() }},
+      color: "#00FF00"
+    }
+  ];
+
+  var camp{{ $campaign->id }} = $.plot($("#campaign{{ $campaign->id}}pie"), camp{{ $campaign->id }}dataset, {
+    series: {
+      pie: {
+        innerRadius: 0.5,
+        show: true,
+        label: {
+          show: false
+        }
+      }
+    },
+    legend: {
+      show: false
+    }
+  });
+@endforeach
+
+
+  window.setInterval(grabActivityLog, 2000);
+});
+
+var latestActivityLog = {{ ($activitylog->count() > 0)?$activitylog[0]->id:-1 }};
+
+function grabActivityLog()
+{
+  $.get("{{ action('AjaxController@get_activitylog') }}/"+latestActivityLog, function(results) {
+    for (var x = 0; x < results.data.length; ++x)
+    {
+      $("#activityLog").prepend(results.data[x] + "<br />");
+    }
+    latestActivityLog = results.latest_id;
+  });
+}
 </script>
 @endsection
