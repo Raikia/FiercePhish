d74069455c934226bb83648a0dec7388adb594b5
Raikia <raikiasec@gmail.com>
2017-01-04 15:38:18 +0000

    Added a better maintenance page, and an Apache 404 page which fixes #10

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,6 +4,4 @@
 </head><body>
 <h1>Not Found</h1>
 <p>The requested URL {{ $_SERVER['REQUEST_URI'] }} was not found on this server.</p>
-<hr>
-<address>Apache/2.4.7 (Ubuntu) Server at {{ $_SERVER['SERVER_NAME'] }} Port {{ (!empty($_SERVER['HTTPS']))?'443':'80' }}</address>
 </body></html>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html>
     <head>
         <title>Be right back.</title>
@@ -44,4 +44,33 @@
             </div>
         </div>
     </body>
-</html>
+</html>-->
+
+
+<!DOCTYPE html><html class=''>
+<head>
+  <meta charset='UTF-8'>
+  <meta name="robots" content="noindex">
+
+
+<link href="{{ asset('css/login_page.css') }}" rel="stylesheet">
+<title>{{ config('firephish.APP_NAME') }} &raquo; Maintenance Mode</title>
+<style type="text/css">
+    h1 {
+    left: 0;
+    line-height: 200px;
+    margin: auto;
+    margin-top: -100px;
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 32pt;
+}
+</style>
+</head>
+<body>
+<h1>Maintenance mode</h1>
+
+</body></html>
