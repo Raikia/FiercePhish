434c4b0da94886504125f0b9595220c54f086d22
Raikia <raikiasec@gmail.com>
2017-01-09 12:27:53 +0000

    Changed count of TargetUser to be more efficient

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -50,7 +50,7 @@
   <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="tile-stats">
       <div class="icon"><i class="fa fa-user-o"></i></div>
-      <div class="count">{{ App\TargetUser::all()->count() }}</div>
+      <div class="count">{{ App\TargetUser::count() }}</div>
       <h3>Total Users</h3>
       <p>Number of target users</p>
     </div>
