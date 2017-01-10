08714206f0bb9f17aa0c7e2135661229a4493ddf
Raikia <raikiasec@gmail.com>
2017-01-10 08:48:05 +0000

    Significantly increased the load time of assign user to list

--- resources/views/targets/assign.blade.php
+++ resources/views/targets/assign.blade.php
@@ -91,7 +91,7 @@
             <div class="col-md-9 col-sm-9 col-xs-12">
                 <input type="hidden" name="listSelection" value="{{ $selectedList->id }}" />
                 <input type="hidden" name="type" value="edit" />
-                <input type="text" class="form-control" id="listSelection" value="{{ $selectedList->name}} - {{ count($selectedList->users) }} users" readonly />
+                <input type="text" class="form-control" id="listSelection" value="{{ $selectedList->name}} - {{ $selectedList->users()->count() }} users" readonly />
             </div>
           </div>
           
