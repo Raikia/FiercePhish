27afd031873005485d1a95fb55e92f2718189c0a
Raikia <raikiasec@gmail.com>
2017-01-11 02:06:40 +0000

    Space change

--- resources/views/campaigns/create.blade.php
+++ resources/views/campaigns/create.blade.php
@@ -16,7 +16,7 @@
           <form class="form-horizontal form-label-left input_mask">
 
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12">Campaign Name <span class="required">*</span></label>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Campaign Name&nbsp;<span class="required">*</span></label>
                 <div class="col-md-9 col-sm-9 col-xs-12">
                   <input type="text" class="form-control" id="campaign_name_raw" placeholder="">
                 </div>
