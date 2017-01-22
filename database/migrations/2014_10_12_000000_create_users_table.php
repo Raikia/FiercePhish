e567eafd981ce1352b882c01b8bd2463c7819bb1
Raikia <raikiasec@gmail.com>
2017-01-22 07:50:47 +0000

    Added phone_number back to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name')->unique();
             $table->string('email');
+            $table->string('phone_number');
             $table->string('google2fa_secret')->nullable();
             $table->string('password');
             $table->rememberToken();
