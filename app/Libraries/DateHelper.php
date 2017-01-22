dad6fc62c6687ffc0913cbe26ce3768ca821005f
Raikia <raikiasec@gmail.com>
2017-01-22 11:10:13 +0000

    Added readable and format functions to DateHelper

--- app/Libraries/DateHelper.php
+++ app/Libraries/DateHelper.php
@@ -10,9 +10,19 @@ class DateHelper
         return $date->timezone(config('fiercephish.APP_TIMEZONE'))->toDateTimeString();
     }
     
+    public static function readable($date)
+    {
+        return $date->timezone(config('fiercephish.APP_TIMEZONE'))->format('M j, Y @ g:i:s a');
+    }
+    
     public static function relative($date)
     {
         return $date->timezone(config('fiercephish.APP_TIMEZONE'))->diffForHumans();
+    }
+    
+    public static function format($date, $format)
+    {
+        return $date->timezone(config('fiercephish.APP_TIMEZONE'))->format($format);
     }
     
     public static function isNull($date)
