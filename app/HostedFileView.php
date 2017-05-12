ba89e8b119f0f37b02cd38e638eef6b1887b3f2f
Raikia <raikiasec@gmail.com>
2017-05-12 21:14:10 +0000

    Added IP as a fillable option for HostedFileView

--- app/HostedFileView.php
+++ app/HostedFileView.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HostedFileView extends Model
 {
-    protected $fillable = ['hosted_file_id', 'browser', 'browser_version', 'platform', 'useragent', 'uuid', 'referer'];
+    protected $fillable = ['hosted_file_id', 'browser', 'browser_version', 'platform', 'useragent', 'uuid', 'referer', 'ip'];
     
     public function hostfile()
     {
