3e57c173b7f9c80251423b0a13096131c1783444
Raikia <raikiasec@gmail.com>
2017-05-12 21:13:14 +0000

    Added generateGraphData to GlobalHelper to reuse the code needed to generate timetable graphs

--- app/Libraries/GlobalHelper.php
+++ app/Libraries/GlobalHelper.php
@@ -1,10 +1,39 @@
 <?php
 namespace App\Libraries;
 
+use Carbon\Carbon;
+use \App\Libraries\DateHelper;
+
 class GlobalHelper
 {
     public static function makeUnclickableLink($str)
     {
         return str_replace(['http://', 'https://', '.'], ['hxxp://', 'hxxps://', '[.]'], $str);
     }
+    
+    public static function generateGraphData($queryable, $datetype='created_at')
+    {
+        $rawData = $queryable->orderby($datetype, 'asc')->get();
+        $graphData = [];
+        if (count($rawData) > 0)
+        {
+            $dayIterator = DateHelper::convert($rawData[0]->$datetype);
+            $today = DateHelper::now();
+            $graphData = [];
+            $dataCounter = 0;
+            while ($dayIterator->lt($today))
+            {
+                $count = 0;
+                while ($dataCounter < $rawData->count() && DateHelper::convert($rawData[$dataCounter]->$datetype)->isSameDay($dayIterator))
+                {
+                    $count += 1;
+                    $dataCounter += 1;
+                }
+                $graphData[] = [$dayIterator->format('Y-m-d'), $count];
+                $dayIterator = $dayIterator->addDay(1);
+            }
+        }
+        return $graphData;
+    }
 }
+
