e6c32e83764d86b79f02b56bf501b5da920e6121
Raikia <raikiasec@gmail.com>
2017-06-13 16:46:50 +0000

    Added catch for Html2Text in case the HTML is unable to be parsed for some reason

--- resources/views/layouts/email_plaintext.blade.php
+++ resources/views/layouts/email_plaintext.blade.php
@@ -1,1 +1,7 @@
-{!! Html2Text\Html2Text::convert(preg_replace('/(<[^>]+) id=".*?"/i', '$1', $data)) !!}
+@php
+try {
+        echo Html2Text\Html2Text::convert(preg_replace('/(<[^>]+) id=".*?"/i', '$1', $data));
+} catch (\Exception $e) {
+        echo strip_tags($data);
+}
+@endphp
