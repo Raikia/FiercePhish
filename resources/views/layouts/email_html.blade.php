5366f5e6eabd9e023ec9092f3e3d0fd91abec373
Raikia <raikiasec@gmail.com>
2017-06-13 15:50:40 +0000

    Ensuring old templates dont break

--- resources/views/layouts/email_html.blade.php
+++ resources/views/layouts/email_html.blade.php
@@ -1,1 +1,13 @@
+@if (stripos($data, '<html>') === false)
+<html>
+@endif
+@if (stripos($data, '<body>') === false)
+<body>
+@endif
 {!! $data !!}
+@if (stripos($data, '</body>') === false)
+</body>
+@endif
+@if (stripos($data, '</html>') === false)
+</html>
+@endif
