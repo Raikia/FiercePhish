0cc94bb6f288093cdc8196a04c00f47d8db303d6
Raikia <raikiasec@gmail.com>
2017-10-23 00:11:42 +0000

    Updated default styling of laravel to conform to style guide

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        
     ];
 
     /**
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,22 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
+    ];
+}
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
         
     ];
 }
