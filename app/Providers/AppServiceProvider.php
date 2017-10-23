5e9f1ff57b93390061ea96a50cdcb9c99f262a74
Raikia <raikiasec@gmail.com>
2017-10-23 00:10:00 +0000

    Updated default styling of laravel to conform to style guide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        
     }
 
     /**
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        
     }
 }
